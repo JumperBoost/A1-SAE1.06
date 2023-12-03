d09c6bc1701d8b0504ec27a2c67b3594c8125c20
JumperBoost <milwenn59@sfr.fr>
2023-12-03 05:18:41 +0100

    Finalisation de la barre de navigation, Finalisation de la mise en page générale & Avancement sur la page "Contact"

--- src/js/style.js
+++ src/js/style.js
@@ -2,14 +2,30 @@
     Menu Hamburger
 */
 
-const puce = document.querySelector(".puce");
+const puces = document.querySelectorAll(".hamparent > div:first-child > img");
 
-puce.addEventListener('click', function() {
-    const hamenu = this.parentElement.nextElementSibling;
-    if(hamenu.style.display === 'flex') {
-        hamenu.style.display = 'none';
-    } else {
-        hamenu.style.display = 'flex';
-        this.querySelector('')
-    }
+puces.forEach(puce => {
+    puce.addEventListener('click', function() {
+        const hamenu = this.parentElement.nextElementSibling;
+
+        if(hamenu.style.display === 'flex') {
+            hamenu.style.display = 'none';
+            this.style.animation = 'puce 0.3s linear alternate-reverse forwards';
+        } else {
+            hamenu.style.display = 'flex';
+            this.style.animation = 'puce 0.3s linear alternate forwards';
+        }
+    });
+
+    puce.addEventListener('animationend', function(event) {
+        if(event.animationName === 'puce') {
+            if(event.target.parentElement.nextElementSibling.style.display === 'flex') {
+                event.target.style.rotate = '180deg';
+                event.target.style.animation = '';
+            } else {
+                event.target.style.rotate = '90deg';
+                event.target.style.animation = '';
+            }
+        }
+    });
 });
