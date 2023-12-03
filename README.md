b6a7252abfc17315952294b46302ab696e0de328
JumperBoost <milwenn59@sfr.fr>
2023-12-04 00:11:29 +0100

    Finalisation du site web

--- README.md
+++ README.md
@@ -7,5 +7,14 @@ Projet Controverse - SAE 1.06 | Année 1 | IUT Montpellier-Sète
 - Enzo PIGNANELLI
 - Amine BEN ROMDHANE
 
+# Implication de chaque Membres
+**Chaque membre a travaillé de manière égale (à part Amine), et s'est entraîdé avec les autres membres du groupe. Les implications suivantes sont écrites par ordre de priorité.**
+
+
+- Milwenn FRANCEUS--COINTREL (33%) : Page Accueil, pages acteurs, barre de navigation, PESTEL
+- Héloïse RIGAUX (33%) : Barre de navigation, Pages impacts, Page contact
+- Enzo PIGNANELLI (33%) : Page contact, Page footer, PESTEL
+- Amine BEN ROMDHANE (0%) : Absent
+
 # Lien vers la page web
 https://webinfo.iutmontp.univ-montp2.fr/~franceusm/CTRV_SAE06/
--- src/js/style.js
+++ src/js/style.js
@@ -2,12 +2,17 @@
     Menu Hamburger
 */
 
+// On récupère toutes les puces
 const puces = document.querySelectorAll(".hamparent > div:first-child > img");
 
+// On parcourt toutes les puces, et on ajoute un écouteur d'événement sur chacune d'entre elles
 puces.forEach(puce => {
+    // On ajoute un écouteur d'événement sur le click de la puce
     puce.addEventListener('click', function() {
+        // On récupère le menu hamburger, qui est le frère suivant de la puce
         const hamenu = this.parentElement.nextElementSibling;
 
+        // On affiche ou on cache le menu hamburger en fonction de son état actuel
         if(hamenu.style.display === 'flex') {
             hamenu.style.display = 'none';
             this.style.animation = 'puce 0.3s linear alternate-reverse forwards';
@@ -17,13 +22,15 @@ puces.forEach(puce => {
         }
     });
 
+    // On ajoute un écouteur d'événement sur la fin de l'animation de la puce
     puce.addEventListener('animationend', function(event) {
+        // Si l'animation est terminée, et que c'est l'animation 'puce' qui est terminée, on fait pivoter la puce
         if(event.animationName === 'puce') {
             if(event.target.parentElement.nextElementSibling.style.display === 'flex') {
-                event.target.style.rotate = '180deg';
+                event.target.style.transform = 'rotate(180deg)';
                 event.target.style.animation = '';
             } else {
-                event.target.style.rotate = '90deg';
+                event.target.style.transform = 'rotate(90deg)';
                 event.target.style.animation = '';
             }
         }
